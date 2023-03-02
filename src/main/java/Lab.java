7c5533029523d226972bd716c387ec9ed7638d26
Wesley Edwards <wredwards94@outlook.com>
2023-03-02 11:10:56 +0000

    lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,6 @@ public class Lab {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-        return Math.pow(a, b);
+        return Math.pow(c, b);
     }
 }
